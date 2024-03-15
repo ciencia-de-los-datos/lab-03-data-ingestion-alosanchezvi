d1cb2e235e6a7dd5a904f274d1fbb1ab73496dfe
alosanchezvi <alosanchezvi@unal.edu.co>
2024-03-15 10:11:12 -0500

    Update

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,49 @@ espacio entre palabra y palabra.
 
 
 """
+import re
 import pandas as pd
 
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+
+    pattern = r'(\d+)\s+(\d+)\s+([\d,]+\s%\s+)\s+(.*\s*)'
+    path='clusters_report.txt'
+    with open(path, 'r') as f:
+        content = f.read()
+    lineas = content.split('\n')
+
+    indices_numeros=[]
+    for i,elemento in enumerate(lineas):
+        if any(caracter.isdigit() for caracter in elemento):
+            indices_numeros.append(i)
+
+    lista_concat=[]    
+    for a,b in zip(indices_numeros, indices_numeros[1:]+[100]):
+        lista_concat.append(' '.join(lineas[a:b]))
+
+    contenido=[]
+    for i in (lista_concat):
+        contenido.append(re.findall(pattern, i))
+    contenido001=[list(cont[0]) for cont in  contenido]
+    for cont in range(len(contenido001)):
+        contenido001[cont][3]=re.sub('\s+', ' ', contenido001[cont][3]).strip()
+
+    columnas=lineas[:2]
+    nombre_columnas=columnas[0].split('  ')[:2]+columnas[0].split('  ')[3:-2]
+    nombre_columnas[1]=nombre_columnas[1]+' palabras clave'
+    nombre_columnas[2]=nombre_columnas[2]+' palabras clave'
+    nombre_columnas=[i.lower().strip().replace(' ','_') for i in nombre_columnas]
+
+    df=pd.DataFrame(contenido001,columns=nombre_columnas)
+    df['cluster']=df.cluster.astype('int')
+    df['cantidad_de_palabras_clave']=df.cantidad_de_palabras_clave.astype('int')
+    df['porcentaje_de_palabras_clave']=df['porcentaje_de_palabras_clave'].str.replace('%', '').str.replace(',', '.').astype(float)
+    df['principales_palabras_clave']=df['principales_palabras_clave'].astype(str).str.replace('.','')
+ 
+
 
     return df
+
+
